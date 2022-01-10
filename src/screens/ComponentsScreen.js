bab5c534082d53213c8968f1eb9e19a7a8e50535
Sourabh Gangadharan <sourabhg@quickplay.com>
2022-01-10 18:40:05 +0530

    Component screen added

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, StyleSheet,View,FlatList} from 'react-native';
+const ComponentsScreen = ()  => {
+    const greeting = <Text>HELLO TO YOU</Text>;
+    return(
+        <View>
+         <Text style={styles.textStyle}>
+        THIS IS THE COMPONENT SCREEN </Text>
+        <Text style={styles.subHeaderStyle}>
+        THIS IS THE subheaderstyle </Text>
+        
+        {greeting}  
+        </View>
+    ) ;  
+};
+const styles =StyleSheet.create({
+    textStyle:{
+        fontSize:30
+    },
+    subHeaderStyle:{
+        fontSize:45
+    }
+    
+});
+export default ComponentsScreen;
