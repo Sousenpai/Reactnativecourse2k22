5ea61e13c4536032cc48123bd4469cb3ad4d5fcc
Sourabh Gangadharan <sourabhg@quickplay.com>
2022-01-17 16:26:31 +0530

    proj complete screens added

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Text, StyleSheet,View,FlatList} from 'react-native';
-const ComponentsScreen = ()  => {
+import { Text, StyleSheet, View, FlatList } from 'react-native';
+const ComponentsScreen = () => {
     const greeting = <Text>HELLO TO YOU</Text>;
-    return(
+    return (
         <View>
-         <Text style={styles.textStyle}>
-        THIS IS THE COMPONENT SCREEN </Text>
-        <Text style={styles.subHeaderStyle}>
-        THIS IS THE subheaderstyle </Text>
-        
-        {greeting}  
+            <Text style={styles.textStyle}>
+                THIS IS THE COMPONENT SCREEN </Text>
+            <Text style={styles.subHeaderStyle}>
+                THIS IS THE subheaderstyle </Text>
+
+            {greeting}
         </View>
-    ) ;  
+    );
 };
-const styles =StyleSheet.create({
-    textStyle:{
-        fontSize:30
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 30
     },
-    subHeaderStyle:{
-        fontSize:45
+    subHeaderStyle: {
+        fontSize: 45
     }
-    
+
 });
 export default ComponentsScreen;
